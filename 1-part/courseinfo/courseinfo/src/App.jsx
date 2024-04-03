7b3ad7fe0e2b84616d063e18cda50109dd92acbe
WizardLord160 <wizardlord160@gmail.com>
2024-04-03 14:33:41 -0700

    Finished exercises 1.1-1.2

--- 1-part/courseinfo/courseinfo/src/App.jsx
+++ 1-part/courseinfo/courseinfo/src/App.jsx
@@ -6,18 +6,22 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
-        {props.part1} {props.exercises1}
+        {props.part} {props.exercise}
       </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercise={props.exercises1}/>
+      <Part part={props.part2} exercise={props.exercises2}/>
+      <Part part={props.part3} exercise={props.exercises3}/>
     </>
   )
 }
@@ -40,7 +44,11 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content 
+        part1={part1} exercises1={exercises1} 
+        part2={part2} exercises2={exercises2} 
+        part3={part3} exercises3={exercises3}
+      />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
