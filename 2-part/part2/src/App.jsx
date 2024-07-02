e612f20d236cd980c64125c7407205d7bd44cc55
Dylan Lim <35948264+WizardLord160@users.noreply.github.com>
2024-07-01 18:52:56 -0700

    ESLint and proptypes

--- 2-part/part2/src/App.jsx
+++ 2-part/part2/src/App.jsx
@@ -1,7 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
+
 import Note from './components/Note'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import LoginForm from './components/LoginForm'
+import NoteForm from './components/NoteForm'
+
 import noteService from './services/notes'
 import loginService from './services/login'
 import './index.css'
@@ -22,14 +27,18 @@ const Footer = () => {
 }
 
 const App = () => {
+  const [loginVisible, setLoginVisible] = useState(false)
+
   const [notes, setNotes] = useState(null)
-  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true) // Showing all notes
   const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
+
+  // Reference to Toggable component that controls NoteForm
+  const noteFormRef = useRef()
 
 
   useEffect(() => {
@@ -79,24 +88,13 @@ const App = () => {
       })
   }
 
-  const addNote = (event) => {
-    event.preventDefault() // prevent default form behavior, which would refresh page
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-    }
-  
+  const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility()
     noteService
       .create(noteObject)
       .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
-        setNewNote('')
       })
-  }
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
   }
 
   const notesToShow = showAll
@@ -142,43 +140,34 @@ const App = () => {
     console.log(user)
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  )
-
-  const noteForm = () => (
-    <form onSubmit={addNote}>
-      <input
-        value={newNote}
-        onChange={handleNoteChange}
-      />
-      <button type="submit">save</button>
-    </form>  
-  )
-
   const LogoutButton = ({handleLogout}) => (
     <button onClick={handleLogout}>log out</button>
   )
+
+  const loginForm = () => {
+    // if login visible, hide element that has this style
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    // if login invisible, show element that has the style
+    const showWhenVisible = { display: loginVisible ? '' : 'none' }
+
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>log in</button>
+        </div>
+        <div style={showWhenVisible}>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -189,7 +178,9 @@ const App = () => {
       {!user && loginForm()}
       {user && <div>
         <p>{user.name} logged in <LogoutButton handleLogout={handleLogout}/></p>
-          {noteForm()}
+        <Togglable buttonLabel="new note" ref={noteFormRef}>
+          <NoteForm createNote={addNote}/>
+        </Togglable>
         </div>
       } 
 
--- 2-part/part2/src/components/Togglable.jsx
+++ 2-part/part2/src/components/Togglable.jsx
@@ -1,7 +1,10 @@
-import { useState, useImperativeHandle, forwardRef } from 'react'
+import { useState, forwardRef, useImperativeHandle } from 'react' 
 import PropTypes from 'prop-types'
 
-const Togglable = forwardRef((props, ref) => {
+// Function creating component wrapped in forwardRef function call so that component that access ref assigned to it
+const Togglable = forwardRef((props, refs) => {
+  // Reusable toggable component to create elements that can be toggled to be shown and hidden
+
   const [visible, setVisible] = useState(false)
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
@@ -11,13 +14,16 @@ const Togglable = forwardRef((props, ref) => {
     setVisible(!visible)
   }
 
-  useImperativeHandle(ref, () => {
+  // Makes toggleVisibility function available outside component
+  useImperativeHandle(refs, () => {
     return {
       toggleVisibility
     }
   })
 
   return (
+    // Forms a togglable parent component
+    // Anything in between the opening and closing tags of this parent component (togglable) is the child component and goes where props.children is
     <div>
       <div style={hideWhenVisible}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
@@ -33,5 +39,7 @@ const Togglable = forwardRef((props, ref) => {
 Togglable.propTypes = {
   buttonLabel: PropTypes.string.isRequired
 }
+
+Togglable.displayName = 'Togglable'
 
 export default Togglable
