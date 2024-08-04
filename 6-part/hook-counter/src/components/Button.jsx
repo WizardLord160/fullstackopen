f3ec781ea2f25f26ec5563bed1a0837f4a770e71
Dylan Lim <35948264+WizardLord160@users.noreply.github.com>
2024-08-03 19:50:44 -0700

    Exercise 6.23-6.24

---
+++ 6-part/hook-counter/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import { useCounterDispatch } from '../CounterContext'
+
+const Button = ({ type, label }) => {
+  const dispatch = useCounterDispatch()
+  return (
+    <button onClick={() => dispatch({ type })}>
+      {label}
+    </button>
+  )
+}
+
+export default Button
---
+++ 6-part/hook-counter/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+import { CounterContextProvider } from './CounterContext'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <CounterContextProvider>
+    <App />
+
+  </CounterContextProvider>
+)
